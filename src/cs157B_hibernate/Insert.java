7206d62bd0547fb4429a29e167aa76b4dd589f69
jennifernghinguyen <jennifernghi@yahoo.com>
2018-05-07 07:57:36 -0700

    add comments

--- src/cs157B_hibernate/Insert.java
+++ src/cs157B_hibernate/Insert.java
@@ -6,19 +6,18 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
-
 public class Insert {
-
+	/**
+	 * read data from sales_data.txt and insert into sales table
+	 */
 	public static void insertData() {
-		try {
+		try {			
 			Scanner scanner = new Scanner(new File("src/cs157B_hibernate/sales_data.txt"));
 			while(scanner.hasNextLine()){
-				String[] next = scanner.nextLine().split("\\s+");
+				String[] next = scanner.nextLine().split("\\s+"); //slit all the spaces
 				
 				try {
-					
 					Date date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(next[0] + " " +next[1]);
-					
 					
 					String productName = next[2];
 					
@@ -26,25 +25,20 @@ public class Insert {
 					
 					double unitCost = Double.parseDouble(next[4]);
 					
-					double productCost = quantity * unitCost;
-					
+					double productCost = quantity * unitCost; // productCost = quantity * unitCost
+
 					Sales s = new Sales(date, productName, quantity, unitCost, productCost);
 					s.save();
 					
 					
 				} catch (ParseException e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				
 			}
-			
-			scanner.close();
-			
+			scanner.close();		
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 }
